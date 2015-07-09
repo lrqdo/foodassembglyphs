29f8bd6be915cc41b8b840316d5dada4ec1f714d
Erik Escoffier <hello@nerik.me>
2015-07-09 14:37:21 +0200

    configurable paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 'use strict';
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+const gulp   = require('gulp');
+const sass   = require('gulp-sass');
 const insert = require('gulp-insert');
 const config = require('./config');
+const path   = require('path');
+
 
 gulp.task('font', function generateFont(cb) {
     require('./index.js')(cb);
 });
 
 gulp.task('css', ['font'], function() {
-    gulp.src('build/sass/main.scss')
+    gulp.src(path.join(config.pathDestBase, config.pathDestScss, 'main.scss'))
         .pipe(sass().on('error', sass.logError))
         .pipe(insert.prepend(`/*${config.generatedWarning}*/\n`))
         .pipe(gulp.dest('build/specimen'));
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 'use strict';
 
-const path        = require('path');
-const _           = require('underscore');
-const consolidate = require('gulp-consolidate');
-const gulp        = require('gulp');
-const iconfont    = require('gulp-iconfont');
-const merge       = require('merge-stream');
-const onFinished  = require('finished');
-const insert      = require('gulp-insert');
-const config      = require('./config');
+const path          = require('path');
+const _             = require('underscore');
+const consolidate   = require('gulp-consolidate');
+const gulp          = require('gulp');
+const iconfont      = require('gulp-iconfont');
+const merge         = require('merge-stream');
+const onFinished    = require('finished');
+const insert        = require('gulp-insert');
+const defaultConfig = require('./config');
 
-function generateFont(cb) {
-    let paths = _.clone(config.taskPaths);
+function generateFont(cb, customConfig) {
+    // let paths = _.clone(config.taskPaths);
 
-    Object.keys(paths).forEach(function(key) {
-        paths[key] = path.join(__dirname, paths[key]);
+    // Object.keys(paths).forEach(function(key) {
+    //     paths[key] = path.join(__dirname, paths[key]);
+    // });
+    let config = _.extend( _.clone(defaultConfig), customConfig);
+
+    ['Svg', 'Fonts', 'Scss', 'HtmlSpecimen'].forEach(function(key) {
+        config['pathSrc' + key] = path.join(config.pathSrcBase, config['pathSrc' + key] || '');
+        config['pathDest' + key] = path.join(config.pathDestBase, config['pathDest' + key] || '');
     });
 
     function buildTemplates(glyphs) {
@@ -22,29 +28,29 @@ function generateFont(cb) {
                 glyphs: glyphs,
         }, config);
 
-        let scssStream = gulp.src(paths.scssTpl)
+        let scssStream = gulp.src(config.pathSrcScss)
             .pipe(consolidate('underscore', templateData))
             .pipe(insert.prepend(`//${config.generatedWarning}\n`))
-            .pipe(gulp.dest(paths.scssBuild));
+            .pipe(gulp.dest(config.pathDestScss));
 
-        let htmlStream = gulp.src(paths.htmlTpl)
+        let htmlStream = gulp.src(config.pathSrcHtmlSpecimen)
             .pipe(consolidate('underscore', templateData))
             .pipe(insert.prepend(`<!--${config.generatedWarning}-->\n`))
-            .pipe(gulp.dest(paths.htmlBuild));
+            .pipe(gulp.dest(config.pathDestHtmlSpecimen));
 
         onFinished(merge(scssStream, htmlStream), function(err) {
-            cb(err, config);
+            cb(err);
         });
     }
 
-    gulp.src([paths.svg])
+    gulp.src([config.pathSrcSvg])
         .pipe(iconfont({
             fontName     : config.fontFaceName,
             appendUnicode: false,
             normalize    : true,
         }))
         .on('glyphs', buildTemplates)
-        .pipe(gulp.dest(paths.fonts));
+        .pipe(gulp.dest(config.pathDestFonts));
 }
 
 module.exports = generateFont;
